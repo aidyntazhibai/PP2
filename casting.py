9c2e14898ed083c8ea65cea6a438772b84fbc84b
1245656 <dmmmaster197@gmail.com>
2023-01-19 23:42:03 +0500

    cast

---
+++ casting.py
@@ -0,0 +1,8 @@
+#in python we can specify var type
+
+x=int(2.0) #so x will  be 2
+x=float("2.1") #so x will be 2.1
+print(x)
+print(type(x))
+x=str(1232323432) #it will be just text
+print(type(x))
