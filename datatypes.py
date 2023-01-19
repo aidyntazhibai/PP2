6497cc78f9943efb8d2b7c1f682e065149abf6c2
1245656 <dmmmaster197@gmail.com>
2023-01-19 23:21:19 +0500

    d

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+#in python data type set automatically
+#there is inbuild datatypes
+x=2 #int
+x="please 1 ball" #str
+x=10.5 #float
+x=1j #complex
+x=["python", "java", "cpp"] #list
+x=("python", "java", "cpp") #tuple
+x=range(6)	#range	
+x={"name" : "Aidyn", "age" : 18} #dict	
+x={"python", "java", "cpp"}	#set
+x=True	#bool
+
+#and moreee
+
