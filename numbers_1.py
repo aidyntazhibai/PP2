4162d030f5a7a1415ff5b4f8556ebbec43f226a1
1245656 <dmmmaster197@gmail.com>
2023-01-19 23:37:56 +0500

    a

---
+++ numbers_1.py
@@ -0,0 +1,22 @@
+
+#there is types of numbers
+
+x=int(20) #integers + or -
+x=float(20.3) #float numbers
+x=1j #comlpex
+
+#we can converse them
+
+x=1
+y=1.5
+z=1j
+
+a=float(x) 
+b=int(y)
+c=complex(y)
+
+# complex cannot converse to another num type
+
+#python has inpuild random module
+import random
+print(random.randrange(1, 10))
