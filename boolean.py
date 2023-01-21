7a3e1fdbc2bd9319525fdf2f9286042022633002
1245656 <dmmmaster197@gmail.com>
2023-01-21 23:29:05 +0500

    a

---
+++ boolean.py
@@ -0,0 +1,73 @@
+
+#boolean is operator that shows true or false
+
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a=1
+b=2
+
+if a>b:
+    print("yes")
+else:
+    print("no")
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
